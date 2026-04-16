7af2633956757c5f90d5642c22e040097fc9bf76
John Muñoz <jmunoztopa@gmail.com>
2026-04-16 16:16:25 -0500

    Se ajustan los cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router, // 👈 CAMBIO CLAVE
   Routes,
   Route,
   Navigate,
@@ -11,16 +11,14 @@ import Home from "./pages/Home/Home.jsx";
 import About from "./pages/About/About.jsx";
 import Gallery from "./pages/Gallery/Gallery.jsx";
 import Login from "./pages/Login/login.jsx";
+
 // Dashboard / Admin
 import Admin from "./pages/Admin/Admin.jsx";
 import Contact from "./pages/Contact/Contact.jsx";
 import Category from "./pages/Category/Category.jsx";
 
-// Componente de login simple (puedes personalizarlo)
-
 // Simulación de autenticación
 const isAuthenticated = () => {
-  // Aquí podrías leer un token de localStorage o contexto de auth
   return localStorage.getItem("token") ? true : false;
 };
 
@@ -33,14 +31,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Páginas públicas */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/login" element={<Login />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/category" element={<Category />} />
-        {/* Dashboard protegido */}
         <Route
           path="/admin"
           element={
@@ -50,7 +46,7 @@ function App() {
           }
         />
 
-        {/* Ruta por defecto a home si no existe */}
+    
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
