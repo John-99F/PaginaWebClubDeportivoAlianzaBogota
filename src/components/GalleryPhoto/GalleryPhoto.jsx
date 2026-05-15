1e126ae9bd7f6c2f9f66c5c53e75824932f56912
John Muñoz <jmunoztopa@gmail.com>
2026-05-14 22:32:13 -0500

    Se realizo ajuste

--- src/components/GalleryPhoto/GalleryPhoto.jsx
+++ src/components/GalleryPhoto/GalleryPhoto.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import "./GalleryPhoto.css";
 import futbol1 from "../../assets/img/futbol1.jpg";
-import futbol2 from "../../assets/img/futbol2.jpg";
+import futbol2 from "../../assets/img/Futbol2.jpeg";
 import futbol3 from "../../assets/img/Futbol3.jpg";
+import { getDocument } from "../../services/firestore";
+
 
 export default function GalleryPhoto() {
   const images = [futbol1, futbol2, futbol3];
@@ -17,9 +19,41 @@ export default function GalleryPhoto() {
     return () => clearInterval(interval);
   }, [images.length]);
 
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        setLoading(true);
+
+        const contenido = await getDocument("home", "Formacion");
+
+        setData({
+          contenido,
+        });
+      } catch (error) {
+        console.error("Error cargando datos:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    load();
+  }, []);
+
+  useEffect(() => {
+    console.log("data actualizada:", data);
+  }, [data]);
+
+  if (loading) {
+    return (
+      <div>
+      </div>
+    );
+  }
   return (
     <div className="gallery-layout">
-      
       {/* IZQUIERDA - GALERÍA */}
       <div className="gallery-left">
         <div className="gallery">
@@ -31,12 +65,11 @@ export default function GalleryPhoto() {
 
       {/* DERECHA - MENSAJE */}
       <div className="gallery-right">
-        <h2>Formación Integral</h2>
+        <h2>{data.contenido.titulo}</h2>
         <p>
-          Nos dedicamos a construir una generación de futbolistas íntegros, con pasión por el juego y el deseo de dejar huella dentro y fuera de la cancha.
+          {data.contenido.descripcion}
         </p>
       </div>
-
     </div>
   );
 }
--- src/pages/Gallery/GalleryInfo.jsx
+++ src/pages/Gallery/GalleryInfo.jsx
@@ -1,20 +1,31 @@
 import equipo4 from "../../assets/img/Equipo4.jpg";
 import equipo5 from "../../assets/img/Equipo5.jpg";
 import equipo3 from "../../assets/img/Equipo3.jpg";
+import equipo10 from "../../assets/img/Equipo10.jpeg";
+import equipo11 from "../../assets/img/Equipo11.jpg";
+import equipo12 from "../../assets/img/Equipo12.jpg";
+import equipo13 from "../../assets/img/Equipo13.jpeg";
+import equipo14 from "../../assets/img/Equipo14.jpeg";
+import entrenamiento from "../../assets/img/entrenamiento.jpg";
+import equipo15 from "../../assets/img/Equipo15.jpeg";
 import heroImg from "../../assets/img/IMG_0192.jpg";
 
 const GalleryData = {
-    hero: {
-  tituloGallery: "Conocenos",
-  descripcionGallery: "Conoce los momentos destacados de nuestro Club ⚽",
+  hero: {
+    tituloGallery: "Conocenos",
+    descripcionGallery: "Conoce los momentos destacados de nuestro Club ⚽",
     imagenHero: heroImg,
-    },
-
+  },
 
   imagenes: [
-    { id: 1, src: equipo4, titulo: "Entrenamiento en equipo" },
+    { id: 1, src: equipo11, titulo: "Entrenamiento en equipo" },
     { id: 2, src: equipo5, titulo: "Partido amistoso" },
-    { id: 3, src: equipo3, titulo: "Celebración de victoria" },
+    { id: 3, src: equipo10, titulo: "Partido" },
+    { id: 4, src: equipo12, titulo: "Momentos previos" },
+    { id: 5, src: equipo13, titulo: "Momentos previos" },
+    { id: 6, src: equipo14, titulo: "Equipo" },
+    { id: 7, src: entrenamiento, titulo: "Entrenamiento" },
+    { id: 8, src: equipo15, titulo: "Despues del partido" },
   ],
 };
 
